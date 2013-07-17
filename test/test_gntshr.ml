d4d519288d9ce3d0e1dbf5c794fd2c912b60ed0a
Vincent Bernardoff <vincent.bernardoff@citrix.com>
2013-07-17 14:41:17 +0100

    test_gntshr now tests gnttab as well.

--- test/test_gntshr.ml
+++ test/test_gntshr.ml
@@ -1,10 +1,14 @@
 open Gnt
-open Gnt.Gntshr
 
 let main () =
-  let h = interface_open () in
-  let share = share_pages_exn h 0 1 true in
-  List.iter (fun r -> Printf.printf "%d\n%!" (int_of_grant_table_index r)) share.refs;
-  munmap_exn h share
+  let shr_h = Gntshr.interface_open () in
+  let dev_h = Gnttab.interface_open () in
+  let share = Gntshr.share_pages_exn shr_h 0 1 true in
+  List.iter (fun r -> Printf.printf "Shared a page with gntref = %d\n%!" (int_of_grant_table_index r)) Gntshr.(share.refs);
+  Printf.printf "Shared page(s) OK. Now trying to map.\n%!";
+  let local_mapping = Gnttab.map_exn dev_h Gnttab.({domid=0; ref=List.hd Gntshr.(share.refs)}) true in
+  Printf.printf "Mapping OK! Now unmapping and unsharing everything.\n%!";
+  Gnttab.unmap_exn dev_h local_mapping;
+  Gntshr.munmap_exn shr_h share
 
 let _ = main ()
