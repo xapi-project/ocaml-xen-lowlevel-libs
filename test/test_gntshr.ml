d5b31b9f105e174aad45db9ea0d81f3d68d38410
Vincent Bernardoff <vincent.bernardoff@citrix.com>
2013-07-17 15:14:54 +0100

    test_gntshr now tests mapv_exn as well.

--- test/test_gntshr.ml
+++ test/test_gntshr.ml
@@ -9,6 +9,14 @@ let main () =
   let local_mapping = Gnttab.map_exn dev_h Gnttab.({domid=0; ref=List.hd Gntshr.(share.refs)}) true in
   Printf.printf "Mapping OK! Now unmapping and unsharing everything.\n%!";
   Gnttab.unmap_exn dev_h local_mapping;
+  Gntshr.munmap_exn shr_h share;
+  Printf.printf "Now trying to share and map 10 pages as a vector.\n%!";
+  let share = Gntshr.share_pages_exn shr_h 0 10 true in
+  let refs = Gntshr.(share.refs) in
+  let grants = List.map (fun ref -> Gnttab.({domid=0; ref})) refs in
+  let local_mapping = Gnttab.mapv_exn dev_h grants true in
+  Printf.printf "Success! Now unmapping and unsharing everything!\n%!";
+  Gnttab.unmap_exn dev_h local_mapping;
   Gntshr.munmap_exn shr_h share
 
 let _ = main ()
